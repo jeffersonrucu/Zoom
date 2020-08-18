5d9422da904b551953964d1dd191d6330f18d499
Jefferson Oliveira <jefferoidtex7@gmail.com>
2020-08-18 20:38:50 -0300

    Corrige carrouse

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@
 
 $('.owl-carousel').owlCarousel({
     loop:true,
-    margin:10,
     nav:true,
     dots:false,
     items:1,
-    margin:30,
     stagePadding:30,
-    smartSpeed:450
+    smartSpeed:450,
+    animateOut: 'animate__backOutRight',
+    animateIn: 'animate__backInLeft',
 })

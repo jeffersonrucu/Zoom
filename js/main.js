bbb41de959e5ec3bdfd1cfc00c3d675a616caae6
Jefferson Oliveira <jefferoidtex7@gmail.com>
2020-08-18 20:16:57 -0300

    Cria carrouse

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//carroseu
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    items:1,
+    margin:30,
+    stagePadding:30,
+    smartSpeed:450
+})

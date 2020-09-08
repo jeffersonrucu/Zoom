93b971d0913eda062bc317ae6e4eb83e19ba0df6
Jefferson Oliveira <jefferoidtex7@gmail.com>
2020-09-08 20:05:44 -0300

    mesclar

--- js/main.js
+++ js/main.js
@@ -11,20 +11,34 @@ $('.owl-carousel').owlCarousel({
 })
 
 $('.btn_filter').on('click',function(){
-    event.preventDefault()
-    var categoria = $(this).attr('data-type')
+    event.preventDefault();
+    var categoria = $(this).attr('data-type');
+    var card = $(".portfolio1 [data-target='card']");
 
-    if(categoria == 'all') {
-        $('.portfolio div').show()
-    }
-        else {
-        $('.portfolio .card').each(function(){
-            if(!$(this).hasClass(categoria)){
-                $(this).hide()
+    if (categoria == 'all') {
+        card.show();
+    } else {
+        card.each(function(){
+            if ($(this).data('type') !== categoria) {
+                $(this).hide();
+            } else {
+                $(this).show();
             }
-                else{
-                    $(this).show()
-                }
         })
     }
-})
+});
+
+
+//--------------------------------------------------------------------
+
+//$(.class) - seletor de class
+
+//$(#class) - seletor de id
+
+//$('span') - seletor de elementos html
+
+//var type = 'photografy'; - criação de variavel com uma sting atribuida
+
+//$(["data-type='" + type +"'"]) - seletor de attributo concatenado com variavel
+
+//$(["data-type='photografy'"]) - seletor de atributo simples
